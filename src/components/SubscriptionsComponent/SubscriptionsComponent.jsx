6fe5aa2fb94a9f04786ec4bbd6037b34087e726a
codecraftwt <codecraftwt@gmail.com>
2025-04-28 12:12:13 +0530

    Layout Added in components

--- src/components/SubscriptionsComponent/SubscriptionsComponent.jsx
+++ src/components/SubscriptionsComponent/SubscriptionsComponent.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Box} from "@mui/material"
 
 const SubscriptionsComponent = () => {
   return (
-    <div>SubscriptionsComponent</div>
+    <Box
+    sx={{
+      boxSizing: "border-box",
+      width: "100%",
+      height: "85%",
+      padding: "1rem",
+      paddingLeft: "0",
+      paddingTop: "0"
+
+    }}
+  >
+    <Box
+      sx={{
+          borderRadius: "15px",
+          background: "rgba(255, 255, 255, 1)",
+          boxShadow: "0px 4px 15px 0px rgba(0, 0, 0, 0.06)",
+          height: "100%",
+          overflowY:"auto"
+      }}
+    ></Box>
+  </Box>
   )
 }
 
